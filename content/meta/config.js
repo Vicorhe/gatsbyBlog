c3bd4faf4fd3dfae1215fd3d63d4adb07ace9a37
Victor He <victorhe@Victors-MacBook-Pro.local>
2018-10-12 23:27:49 +0800

    Initial Commit

---
+++ content/meta/config.js
@@ -0,0 +1,32 @@
+const colors = require("../../src/styles/colors");
+
+module.exports = {
+  siteTitle: "Victor He Inc.", // <title>
+  shortSiteTitle: "Victor He Inc.", // <title> ending for posts and pages
+  siteDescription: "Activity Tracker, Daily Journel, Blog",
+  siteUrl: "https://gatsby-starter-personal-blog.greglobinski.com",
+  pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "en",
+  // author
+  authorName: "victor he",
+  authorTwitterAccount: "vicorhe",
+  // info
+  infoTitle: "victor he",
+  infoTitleNote: "company site",
+  // manifest.json
+  manifestName: "Victor He Inc.",
+  manifestShortName: "PersonalBlog", // max 12 characters
+  manifestStartUrl: "/",
+  manifestBackgroundColor: colors.background,
+  manifestThemeColor: colors.background,
+  manifestDisplay: "standalone",
+  // contact
+  contactEmail: "placeholder@gmail.com",
+  // social
+  authorSocialLinks: [
+    // { name: "github", url: "https://github.com/vicorhe" }
+    // { name: "twitter", url: "https://twitter.com/vicorhe" },
+    // { name: "facebook", url: "http://facebook.com/victorvanhe" }
+  ]
+};
